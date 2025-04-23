0e7c0f9c0471b1c5c398be6d8bed722133e4782a
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2025-04-23 13:04:40 +0630

    create route

---
+++ devleague-frontend/app/(public)/page.jsx
@@ -0,0 +1,12 @@
+
+import { akshar, inter } from "../fonts";
+
+export default function Home() {
+    return (
+        <>
+            <p className={`text-2xl text-amber-400 text-center ${akshar.className} `}>
+                Hello Next
+            </p>
+        </>
+    );
+}
---
+++ devleague-frontend/app/fonts.js
@@ -0,0 +1,12 @@
+import { Inter, Roboto_Mono, Akshar } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const akshar = Akshar({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
