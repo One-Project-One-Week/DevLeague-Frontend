b582837e7c05b8d59955acdda05b80a3078d572b
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2025-04-23 14:45:55 +0630

    add userlogin

---
+++ devleague-frontend/app/(user)/user/auth/layout.jsx
@@ -0,0 +1,9 @@
+export default function UserAuthLayout(
+    { children }
+) {
+    return (
+        <section className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+            {children}
+        </section>
+    );
+}
---
+++ devleague-frontend/app/(user)/user/auth/login/page.jsx
@@ -0,0 +1,13 @@
+import { LoginForm } from "@/components/login-form"
+
+
+export default async function UserLogin() {
+
+    return (
+
+        <div className="w-full max-w-sm md:max-w-3xl">
+            <LoginForm />
+        </div>
+
+    )
+}
