fb50427a245ff40976a1e84fe59a1d43b7b35311
Shinn Thant Swam Ye <110252746+Pi1998@users.noreply.github.com>
2025-04-24 10:46:43 +0630

    Delete devleague-frontend directory
    Hee Hee

--- devleague-frontend/app/(user)/user/auth/login/page.jsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/login-form"
-
-export default async function UserLogin() {
-    return (
-        <div className="w-full max-w-sm md:max-w-3xl">
-            <LoginForm />
-        </div>
-    )
-}
--- devleague-frontend/components/Sidebar.jsx
+++
@@ -1,5 +0,0 @@
-export default function Sidebar() {
-    return (
-        <h1>side bar</h1>
-    )
-}
